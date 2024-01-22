604e838241976c618d36facdce4ef07038d3173a
Vince Jerald Villamora <1276544+vincejv@users.noreply.github.com>
2024-01-22 10:12:49 +0800

    Optimize for sandybridge

--- include/target.mk
+++ include/target.mk
@@ -208,7 +208,7 @@ LINUX_RECONF_DIFF = $(SCRIPT_DIR)/kconfig.pl - '>' $(call __linux_confcmd,$(filt
 ifeq ($(DUMP),1)
   BuildTarget=$(BuildTargets/DumpCurrent)
 
-  CPU_CFLAGS = -Os -pipe
+  CPU_CFLAGS = -O2 -pipe -march=sandybridge -mtune=sandybridge
   ifneq ($(findstring mips,$(ARCH)),)
     ifneq ($(findstring mips64,$(ARCH)),)
       CPU_TYPE ?= mips64
