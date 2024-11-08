9dd57314a66b34f814280a96ee6ce5dbc1cc7e6a
Rodrigo Balerdi <lanchon@gmail.com>
2024-11-08 17:45:12 -0300

    ipq807x: order devices alphabetically
    Alphabetically sort devices in platform.sh
    
    Signed-off-by: Rodrigo Balerdi <lanchon@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/16505
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -190,9 +190,6 @@ platform_do_upgrade() {
 		CI_ROOTPART="rootfs"
 		emmc_do_upgrade "$1"
 		;;
-	tplink,eap660hd-v1)
-		tplink_do_upgrade "$1"
-		;;
 	redmi,ax6|\
 	xiaomi,ax3600|\
 	xiaomi,ax9000)
@@ -217,6 +214,9 @@ platform_do_upgrade() {
 		CI_ROOTPART="rootfs"
 		CI_DATAPART="rootfs_data"
 		emmc_do_upgrade "$1"
+		;;
+	tplink,eap660hd-v1)
+		tplink_do_upgrade "$1"
 		;;
 	yuncore,ax880)
 		active="$(fw_printenv -n active)"
